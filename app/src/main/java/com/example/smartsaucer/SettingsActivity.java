121205d7c26441660b5ebc4bc6b8a3a5cf38ce16
Hayden Frea <hayden868686@gmail.com>
2019-05-21 10:01:45 -0400

    This is the second commit. I just started to get the bluetooth part working, it connects to the HC-06 and is transferring data but for some reason I am only transferring zero's when I hit the buttons on the app.

--- app/src/main/java/com/example/smartsaucer/SettingsActivity.java
+++ app/src/main/java/com/example/smartsaucer/SettingsActivity.java
@@ -1,13 +1,44 @@
 package com.example.smartsaucer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private Button bluetoothButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+
+        //Makes the Bluetooth Connect Button work
+        bluetoothButton = findViewById(R.id.bluetoothButton);
+        bluetoothButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBluetoothActivity();
+            }
+        });
+
+
+
     }
+
+
+
+    public void openBluetoothActivity() {
+        Intent intent = new Intent(this, BluetoothActivity.class);
+        startActivity(intent);
+
+    }
+
+
+
 }
